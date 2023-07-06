4ba2067af9c1d1e1502b6d070c07500b5238c101
KurumiTokiisaki <fahmed@connect.kellettschool.com>
2023-07-06 11:47:16 +0500

    emergency commit before modifying node_modules

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import Banner from './Banner';
+import Exhibit from './Exhibit';
+import ipify from 'ipify';
+console.log(await ipify());
 
 function App() {
   return (
     <div className="App">
-        <p className="banner">Sextant</p>
-        <p className="exhibit">Exhibit tag!</p>
+        <Banner bannerText="Sextant" />
+        <Exhibit name="Exhibit one">Exhibit one contents</Exhibit>
+        <Exhibit name="Exhibit two">Exhibit two contents</Exhibit>
+        <Exhibit name="Exhibit three">Exhibit three contents</Exhibit>
     </div>
   );
 }
